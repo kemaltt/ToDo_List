3fb10703eadf8271833658caf8df536a7e8d9f86
Kemal <ttnckemal@gmail.com>
2022-01-06 19:09:50 +0100

    edit

--- script.js
+++ script.js
@@ -1,85 +1,80 @@
 const addBtn = document.querySelector("#button");
 const inputText = document.querySelector("#input");
-const ul = document.querySelector(".list-ul")
-const ul2 = document.querySelector(".list-ul2")
-
+const ul = document.querySelector(".list-ul");
+const ul2 = document.querySelector(".list-ul2");
 
 const addListElement = () => {
-    const val = inputText.value
+    const val = inputText.value;
 
     if (val.trim() !== "") {
+        const listEl = document.createElement("li");
+        // listEl.innerText = input.value
+        const newInputText = document.createElement("span");
 
+        newInputText.setAttribute("class", "list-element");
+        newInputText.textContent = val;
 
-        const listEl = document.createElement("li")
-            // listEl.innerText = input.value
-        const newInputText = document.createElement("span")
+        const checkBtn = document.createElement("i");
+        checkBtn.setAttribute(
+            "class",
+            "check-btn btn btn-outline-primary fas fa-check-square "
+        );
+        const delBtn = document.createElement("i");
+        delBtn.setAttribute(
+            "class",
+            "delete-btn btn btn-outline-danger fas fa-trash-alt "
+        );
+        delBtn.setAttribute("style", "font-size:18px");
 
-        newInputText.setAttribute("class", "list-element")
-        newInputText.textContent = val
+        listEl.appendChild(newInputText);
+        listEl.appendChild(checkBtn);
+        listEl.appendChild(delBtn);
 
-
-
-        const checkBtn = document.createElement("i")
-        checkBtn.setAttribute("class", "check-btn btn btn-outline-primary fas fa-check-square ")
-        const delBtn = document.createElement("i")
-        delBtn.setAttribute("class", "delete-btn btn btn-outline-danger fas fa-trash-alt ")
-        delBtn.setAttribute("style", "font-size:18px")
-
-
-        listEl.appendChild(newInputText)
-        listEl.appendChild(checkBtn)
-        listEl.appendChild(delBtn)
-
-        ul.appendChild(listEl)
+        ul.appendChild(listEl);
 
         console.log(listEl);
 
-        const deleteBtn = document.querySelectorAll(".delete-btn")
+        const deleteBtn = document.querySelectorAll(".delete-btn");
         Array.from(deleteBtn).forEach(function(delBtn) {
             delBtn.addEventListener("click", function(e) {
                 // console.log(e)
-                const li = e.target.parentElement
-                    // console.log(li);
-                li.remove()
-            })
-        })
+                const li = e.target.parentElement;
+                // console.log(li);
+                li.remove();
+            });
+        });
 
-        const checkedBtn = document.querySelectorAll(".check-btn")
+        const checkedBtn = document.querySelectorAll(".check-btn");
         Array.from(checkedBtn).forEach(function(chkBtn) {
             chkBtn.addEventListener("click", function() {
 
 
-
                 ul2.appendChild(listEl)
 
-
                 console.log(ul2);
-
-
-
-            })
-        })
-
-
-
+            });
+        });
     } else {
-
-        alert("Please enter a value")
+        alert("Please enter a value");
     }
 
-    inputText.value = ""
-}
+    inputText.value = "";
+};
 
-addBtn.addEventListener("click", addListElement)
+addBtn.addEventListener("click", addListElement);
 input.addEventListener("keyup", function(e) {
     if (e.keyCode === 13) {
-        addListElement()
-        inputText.value = ""
+        addListElement();
+        inputText.value = "";
     }
-})
-const list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+});
+const list = document.querySelector("ul");
+list.addEventListener(
+    "click",
+    function(ev) {
+        if (ev.target.tagName === "LI") {
+            ev.target.classList.toggle("checked");
+        }
+    },
+    false
+);
